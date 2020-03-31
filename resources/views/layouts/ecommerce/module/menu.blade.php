5169d16ba18eb2daa3a9848ffb0416216c91ca16
ricotritanto <rico.tritanto89@gmail.com>
2020-03-31 18:32:30 +0700

    error

--- resources/views/layouts/ecommerce/module/menu.blade.php
+++ resources/views/layouts/ecommerce/module/menu.blade.php
@@ -2,8 +2,19 @@
     <li class="nav-item active">
         <a class="nav-link" href="{{ route('front.index') }}">Home</a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('front.product') }}">Product</a>
+    <li class="nav-item submenu dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" href="{{ route('front.product') }}" role="button" aria-haspopup="true" aria-expanded="false">Product</a>
+        <ul class="dropdown-menu">
+            @forelse ($category as $row)
+            <li class="nav-item">
+                <a href="" class="nav-link">$row->name</a>
+            </li>
+            @empty
+                        <div class="col-md-12">
+                            <h3 class="text-center">Empty Product</h3>
+                        </div>
+            @endforelse
+        </ul>
     </li>
     <li class="nav-item submenu dropdown">
         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Shop</a>
